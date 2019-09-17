78b17c0f98637bc3b2084c7fa0cdcadb0cd8ae53
prabhakarpandey <cypruswebbuilders@gmail.com>
2019-09-17 17:21:33 +0300

    first mygym version

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#loginID").click(function(){
+        $("#loginModal").modal('show');
+    });
+    $("#reserveID").click(function(){
+        $("#reserveModal").modal('show');
+    });
+});
+
